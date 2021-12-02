f2dc5279ed514f6c70d4fbc587843a8c25cd4516
Julie SUN <sun.julie0@gmail.com>
2021-12-02 12:16:52 +0100

    modif login.php

--- securite/login.php
+++ securite/login.php
@@ -1,10 +1,61 @@
 <?php
     session_start();
     $page ='login';
+    $erreur = '';
+    $login = '';
     $methode = $_SERVER['REQUEST_METHOD'];
 
-    if ($methode === 'GET'){
- 
+    if ($methode === 'POST'){
+           // je sécurise les datas dans un autre fichier
+        require 'dsn_secure.php';
+
+        // je crée un objet PDO avec 'new PDO'
+        $refPdo = new PDO($db_dsn, $db_user, $db_psw); 
+        //var_dump($refPdo);
+
+        // déclare et récupère login et psw
+        $login = $_POST['login'];
+        $psw = $_POST['psw'];
+        // les filter_input permettent de filtrer login et mdp, pour mieux sécuriser le login 
+        $login = filter_input(INPUT_POST, 'login', FILTER_SANITIZE_EMAIL);
+        $psw = filter_input(INPUT_POST,'psw',FILTER_SANITIZE_SPECIAL_CHARS);
+
+        //requête trouver le login
+        // $sql = 'SELECT * FROM users WHERE login =\''.$login.'\';';
+        $sql = 'SELECT * FROM users WHERE login=:ident';
+        $stat_user = $refPdo->prepare($sql);
+        $stat_user->bindParam(':ident', $login, PDO::PARAM_STR);
+        $stat_user->execute();
+        //var_dump($sql)
+        // die(); permet de stopper la lecture du php avec 'die'
+
+        //envoi de la requête, un PDO Statement avec 'query'
+        // $stat_user = $refPdo->query($sql);
+        // var_dump($stat_user->fetch(PDO::FETCH_ASSOC));
+
+        //rowCount : retourne nb d'une colonne
+            if ($stat_user->rowCount() == 1){
+            //comparer le $psw avec mdp de la database
+            $user = $stat_user->fetch(PDO::FETCH_ASSOC);
+
+            $mdp = $psw;
+
+                if ($user['psw'] === $psw) {
+                // si connecter alors mémoriser en session le login et ...
+
+                $_SESSION['nom'] = $user['nom'];  
+                $_SESSION['role'] = $user['role'];  
+                header('Location: /index.php');
+                exit();
+            } else { // mot de passe incorrect
+                $erreur = 'Mot de passe erroné';
+            } 
+            } else {  // login non trouvé en base
+                $erreur = 'login erroné';
+                echo $login = ''; 
+                // si login incorrect, on vide input
+            }
+}
 ?>
 <!DOCTYPE html>
 <!--
@@ -38,7 +89,8 @@
         <form method="post" action="login.php">
             <div class="mb-3">
                 <label for="id_login" class="form-label">Votre login</label>
-                <input type="text" name="login" class="form-control" id="id_login" aria-describedby="emailHelp">
+                <!-- cas particulier, on peut écrire sous une autre forme: php echo égale à qque chose, on peut l'écrire sous forme suivante -->
+                <input type="text" value="<?= $login; ?>" name="login" class="form-control" id="id_login" aria-describedby="emailHelp">
             </div>
             <div class="mb-3">
                 <label for="id_psw" class="form-label">Votre mot de passe</label>
@@ -46,9 +98,10 @@
             </div>
             <div>
             <?php
-            if (isset($_SESSION['erreur'])) {
+            // ici, on fait un boolean, si $erreur est vide = true, alors...
+            if ($erreur) {
                echo '<div class="alert alert-danger" role="alert">';
-               echo $_SESSION['erreur'];
+               echo $erreur;
                echo '</div>';
             };
             ?>
@@ -62,55 +115,3 @@
         ?>
     </body>
 </html>
-<?php
-} else {
-    // je sécurise les datas dans un autre fichier
-require 'dsn_secure.php';
-
-// je crée un objet PDO avec 'new PDO'
-$refPdo = new PDO($db_dsn, $db_user, $db_psw); 
-//var_dump($refPdo);
-
-// déclare et récupère login et psw
-$login = $_POST['login'];
-$psw = $_POST['psw'];
-// les filter_input permettent de filtrer login et mdp, pour mieux sécuriser le login 
-$login = filter_input(INPUT_POST, 'login', FILTER_SANITIZE_EMAIL);
-$psw = filter_input(INPUT_POST,'psw',FILTER_SANITIZE_SPECIAL_CHARS);
-
-//requête trouver le login
-// $sql = 'SELECT * FROM users WHERE login =\''.$login.'\';';
-$sql = 'SELECT * FROM users WHERE login=:ident';
-$stat_user = $refPdo->prepare($sql);
-$stat_user->bindParam(':ident', $login, PDO::PARAM_STR);
-$stat_user->execute();
-//var_dump($sql)
-// die(); permet de stopper la lecture du php avec 'die'
-
-//envoi de la requête, un PDO Statement avec 'query'
-// $stat_user = $refPdo->query($sql);
-// var_dump($stat_user->fetch(PDO::FETCH_ASSOC));
-
-//rowCount : retourne nb d'une colonne
-if ($stat_user->rowCount() == 1){
-    //comparer le $psw avec mdp de la database
-  $user = $stat_user->fetch(PDO::FETCH_ASSOC);
-
-  $mdp = $psw;
-
-  if ($user['psw'] === $psw) {
-  // si connecter alors mémoriser en session le login et ...
-
-    $_SESSION['nom'] = $user['nom'];  
-    $_SESSION['role'] = $user['role'];  
-    header('Location: /index.php');
-    } else { // mot de passe incorrect
-        $_SESSION['erreur'] = 'Mot de passe erroné';
-        header('Location: login.php');
-    } 
-    } else {  // login non trouvé en base
-        $_SESSION['erreur'] = 'login erroné';
-    header('Location: login.php');
-    }
-}
-?>
